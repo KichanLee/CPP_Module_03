e4c296f9f5bb05847b058eb750c14dcf7225f569
Kichan Lee <kichlee@c4r10s5.42seoul.kr>
2023-12-21 20:17:34 +0900

    ex01 continue

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -6,6 +6,20 @@
 /*   By: kichlee <kichlee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 16:24:38 by kichlee           #+#    #+#             */
-/*   Updated: 2023/12/21 16:24:38 by kichlee          ###   ########.fr       */
+/*   Updated: 2023/12/21 18:38:33 by kichlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+ private:
+  /* data */
+ public:
+  ScavTrap(std::string Name) : ClapTrap(Name) {
+    HitPoints = 100;
+    EnergyPoints = 50;
+    AttackDamage = 20;
+  }
+  ~ScavTrap();
+};
